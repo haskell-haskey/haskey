8397629eae62dbb41cfc13a229a52120385f77c6
Henri Verroken <hverr@users.noreply.github.com>
2017-06-19 17:25:17 +0200

    Enable Travis CI (#1)
    * Add Travis configuration file
    * Remove old GHC versions from Travis
    * Add files for vim and stack workflow
    * Add synopsis and description to cabal file
    * Add source-repository to cabal file
    * Fix haddock documentation
    * Add missing Control.Applicative import
    * Explicitly use vector length when needed
    * Add build badge to readme

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -3,6 +3,8 @@
 
 module Main (main) where
 
+import Control.Applicative ((<$>))
+
 import Data.BTree.Primitives.Index
 import Data.BTree.Primitives.Key
 
@@ -21,7 +23,7 @@ default (Int64)
 instance (Key k, Arbitrary k, Arbitrary v) => Arbitrary (Index k v) where
   arbitrary = do
       keys <- V.fromList . nub <$> orderedList
-      vals <- V.fromList <$> vector (length keys + 1)
+      vals <- V.fromList <$> vector (V.length keys + 1)
       return (Index keys vals)
   shrink (Index keys vals) =
       [ Index newKeys newVals

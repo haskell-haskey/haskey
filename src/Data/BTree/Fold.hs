0f060636f07f6e8de322d92f6e76721b06c52621
Henri Verroken <henriverroken@gmail.com>
2017-06-21 16:04:01 +0200

    Add foldMap and toList implementation for the impure Tree

--- src/Data/BTree/Fold.hs
+++ src/Data/BTree/Fold.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE GADTs #-}
 module Data.BTree.Fold where
 
+import Prelude hiding (foldr, foldl)
+
 import Data.BTree.Alloc.Class
 import Data.BTree.Primitives
 
 import Control.Applicative ((<$>))
 import Control.Monad (liftM2)
 
+import Data.Monoid (Monoid, (<>), mempty)
+
 import qualified Data.Foldable as F
 import qualified Data.Map as M
 
 --------------------------------------------------------------------------------
 
-{-| Perform a right fold over the tree -}
+{-| Perform a right-associative fold over the tree. -}
 foldr :: (AllocM m, Key k, Value a)
       => (a -> b -> b) -> b -> Tree k a -> m b
 foldr _ x (Tree _ Nothing) = return x
@@ -26,5 +30,15 @@ foldrNode :: (AllocM m, Key k, Value a)
           => (m a -> m b -> m b) -> m b -> Height h -> Node h k a -> m b
 foldrNode f x _ (Leaf items) = M.foldr f x (return <$> items)
 foldrNode f x h (Idx idx) = F.foldr (\nid x' -> foldrId f x' (decrHeight h) nid) x idx
+
+--------------------------------------------------------------------------------
+
+foldMap :: (AllocM m, Key k, Value a, Monoid c)
+      => (a -> c) -> Tree k a -> m c
+foldMap f = foldr ((<>) . f) mempty
+
+toList :: (AllocM m, Key k, Value a)
+      => Tree k a -> m [a]
+toList = foldr (:) []
 
 --------------------------------------------------------------------------------

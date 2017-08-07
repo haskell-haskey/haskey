25588e745d65faa2bc5f3008280793d156a8da5a
Henri Verroken <henriverroken@gmail.com>
2017-08-07 17:05:52 +0200

    Add property test for inserting overflow values

--- tests/Properties/Impure/Insert.hs
+++ tests/Properties/Impure/Insert.hs
@@ -6,6 +6,7 @@ import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Control.Monad ((>=>))
 
 import Data.Int
+import Data.Word (Word8)
 import qualified Data.Map as M
 
 import Data.BTree.Alloc.Debug
@@ -15,6 +16,7 @@ import qualified Data.BTree.Impure as Tree
 tests :: Test
 tests = testGroup "Impure.Insert"
     [ testProperty "insertTreeMany" prop_insertTreeMany
+    , testProperty "insertOverflows" prop_insertOverflows
     ]
 
 prop_insertTreeMany :: [(Int64, Integer)] -> [(Int64, Integer)] -> Bool
@@ -33,3 +35,10 @@ prop_insertTreeMany xs ys = ty1 == ty2
               >>= Tree.toList
 
     insertAll kvs = foldl (>=>) return (map (uncurry insertTree) kvs)
+
+prop_insertOverflows :: M.Map Int64 [Word8] -> Bool
+prop_insertOverflows kvs
+    | v <- evalDebug emptyPages $
+        insertTreeMany kvs Tree.empty
+        >>= Tree.toList
+    = v == M.toList kvs

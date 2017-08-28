3e260c6953ffa7c4679ac7c4f333f6530759646d
Henri Verroken <henriverroken@gmail.com>
2017-08-28 19:42:29 +0200

    Re-enable concurrent readers

--- src/Database/Haskey/Alloc/Concurrent/Database.hs
+++ src/Database/Haskey/Alloc/Concurrent/Database.hs
@@ -209,7 +209,7 @@ transact_ act db = void $ transact act db
 transactReadOnly :: (MonadIO m, MonadMask m, ConcurrentMetaStoreM m, Key key, Value val)
                  => (forall n. (AllocReaderM n, MonadMask m) => Tree key val -> n a)
                  -> ConcurrentDb key val -> m a
-transactReadOnly act db = withRLock (concurrentDbWriterLock db) $
+transactReadOnly act db =
     bracket_ (openConcurrentHandles hnds)
              (closeConcurrentHandles hnds) $
 
